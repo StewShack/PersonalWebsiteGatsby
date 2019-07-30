8bfadcbfacd10537640f09f26e3e7bedb830648b
Dan Stewart <danstewart@live.com>
2019-07-29 22:00:04 -0600

    style: Working on eslint

--- src/components/head.js
+++ src/components/head.js
@@ -1,37 +1,42 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 
-const Head = ({title, description}) => {
-    const data = useStaticQuery(graphql`
-    query {
-        site {
-            siteMetadata {
-                title,
-                themeColor
-            }
-        }
-    }`)
-    
-    return (
-        <Helmet>
-            <meta charset="utf-8" />  
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
-            <meta name="msapplication-tap-highlight" content="no" />
-            <meta name="msapplication-TileColor" content={`${data.site.siteMetadata.themeColor}`} />
-            <meta name="theme-color" content={`${data.site.siteMetadata.themeColor}`} />
-            <meta name="Description" content={`${description}`} />
-            <title>{`${data.site.siteMetadata.title} - ${title}`}</title> 
-            <link rel="author" href="/humans.txt" />
-        </Helmet>
-    )
-}
+const Head = ({ title, description }) => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title,
+        themeColor
+      }
+    }
+  }`);
+
+  return (
+    <Helmet>
+      <meta charset="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="msapplication-TileColor" content={`${data.site.siteMetadata.themeColor}`} />
+      <meta name="theme-color" content={`${data.site.siteMetadata.themeColor}`} />
+      <meta name="Description" content={`${description}`} />
+      <title>{`${data.site.siteMetadata.title} - ${title}`}</title>
+      <link rel="author" href="/humans.txt" />
+    </Helmet>
+  );
+};
 
 Head.propTypes = {
   title: PropTypes.string,
-  description: PropTypes.string
+  description: PropTypes.string,
 };
 
-export default Head
+Head.defaultProps = {
+  title: '',
+  description: '',
+};
+
+export default Head;
